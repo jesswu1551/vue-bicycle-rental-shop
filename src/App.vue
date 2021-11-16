39146c6350ae733ab8f14af58f08bb72689c528f
jess <jiayu1551@gmail.com>
2021-11-16 18:01:29 +0800

    calculate total income

--- src/App.vue
+++ src/App.vue
@@ -24,8 +24,10 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /* text-align: center; */
   color: #2c3e50;
+  margin: auto;
   margin-top: 60px;
+  width: 600px;
 }
 </style>
--- src/components/BicycleRentalShop.vue
+++ src/components/BicycleRentalShop.vue
@@ -1,14 +1,32 @@
 <template>
   <div>
+    <h1>Bicycle Rental Shop</h1>
+    <p>Number of bicycles: {{ number }}</p>
+    <h3>Total income({{ number}} * {{ price }}): {{ subTotal }} $</h3>
   </div>
 </template>
 
 <script>
 export default {
   name: 'BicycleRentalShop',
+  data () {
+    return {
+      number: 20,
+      price: 100,
+    }
+  },
+  computed: {
+    subTotal: function () {
+      return this.number * this.price;
+    }
+  },
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
+  h1:after {
+    content: '🚲';
+    font-weight: normal;
+  }
 </style>
